6da9cf3ceecff62645bc91a382374365e45492db
Agent-K <patelkrishiv1910@gmail.com>
2026-09-07 18:49:30 +0530

    first commit

---
+++ 33_manually_lowercase_change.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main()
+{
+    int i;
+    char temp, sentence[1000];
+
+    printf("\n\nEnter Your String :");
+    fgets(sentence, sizeof(sentence), stdin);
+
+    for (i = 0; sentence[i] != '\0'; i++)
+    {
+        temp = sentence[i];
+
+        if (temp >= 'A' && temp <= 'Z')
+        {
+            sentence[i] += 32;
+        }
+
+        else if (temp >= 'a' && temp <= 'z' || temp == ' ' || temp == '\n' || temp == '\t')
+        {
+        }
+
+        else
+        {
+            printf("\n\nEnter Valid Input.\n");
+
+            return 1;
+        }
+    }
+    for (i = 0; sentence[i] != '\0'; i++)
+    {
+        printf("%c", sentence[i]);
+    }
+    return 0;
+}
