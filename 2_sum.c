78c16b48b780a49dfce2b9b6ee92b4ddc23a7992
Agent-K <patelkrishiv1910@gmail.com>
2026-09-07 10:39:31 +0530

    first commit

---
+++ 2_sum.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int x = 10, y = 20, c = 0;
+    printf("value of x : %d \n", x);
+    printf("value of y : %d \n", y);
+    c = x + y;
+    printf("value of sum : %d", c);
+    return 0;
+}
